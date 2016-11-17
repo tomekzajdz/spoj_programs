ae76f5108242391236e96065363503fbe037666d
Tomek Zajdzinski <tzajdzinski@gmail.com>
2016-11-17 22:40:50 +0100

    poprawki w rozwiazaniu, zadanie zaakceptowwane

--- PRIME_T.cpp
+++ PRIME_T.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 
-
-
 using namespace std;
+const int ROZM = 100000;
 
 int main(){
-int n, a;
-int b;
+
+int n, b;
+int liczby[ROZM];
 int licznik = 0;
+int licznik2 = 0;
 cin >> n;
+while (licznik2 < n) {
+cin >> liczby[licznik2];
+licznik2++;
+}
 while (licznik < n) {
     b=0;
-    cin >> a;
-    if (a%2==0){
+    //cin >> a;
+    if (liczby[licznik]!=2&&liczby[licznik]%2==0||liczby[licznik]==1){
         cout << "NIE" << endl;
         licznik++;
        }
     else{
-        for (int i=3; i*i<a; i++){
-        if (a%i==0){
+        for (int i=3; i*i<=liczby[licznik]; i++){
+        if (liczby[licznik]%i==0){
             cout << "NIE" << endl;
             b++;
             licznik++;
