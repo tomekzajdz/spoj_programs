c2a59a6d4dc6555ff1105cad8acca98df1beddfb
Tomek Zajdzinski <tzajdzinski@gmail.com>
2016-11-18 11:30:06 +0100

    poprawione rozwiazania

--- Dwie_cyfry_silni.cpp
+++ Dwie_cyfry_silni.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 
-long long silnia (int wyr){
+long silnia (int wyr){
     if (wyr==0) return 1;
     else
     return silnia(wyr-1) * wyr;
@@ -13,18 +14,26 @@ const int D=30;
 
 int main(){
 int d, wyraz;
-long long sil;
+long sil;
 long liczba[D];
 cin >> d;
+if(d<=D){
 for ( int i=0; i<d; i++){
     cin >> liczba[i];
 }
 
 for ( int j=0; j<d; j++){
+    if (liczba[j]<10){
     wyraz = liczba[j];
     sil = silnia(wyraz);
     int ltd = sil%100;
-    cout << ltd/10 << " " << ltd%10<<endl;
+    int ltd1 = ltd/10;
+    int ltd2 = ltd%10;
+    cout << ltd1 << " " << ltd2<<endl;
+    }
+    else
+    cout << 0 << " " << 0 << endl;
+}
 }
 
     return 0;
